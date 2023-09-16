755e0aba043989a34d85a96812129a4b7471ab9a
Dhananjaya-dabeer <dabeerdhananja123@gmail.com>
2023-09-17 01:52:40 +0530

    added login/register routes and jwt tokenization

--- User.js
+++ User.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    Name : String,
-    Email : String,
-    Mobile : Number,
-
+    Name :  String, 
+    Email :  String, 
+    Mobile : Number, 
+    Password :  String, 
 })
 
 module.exports = mongoose.model('User',userSchema)
--- node.js
+++ node.js
@@ -2,70 +2,108 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const User = require('./User')
-
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./User");
 
 const app = express();
 dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-
-app.get('/', (req,res) => {
-    res.status(200).send('go to /health to confirm the server is working')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("go to /health to confirm the server is working");
+});
 
 app.get("/health", (req, res) => {
   res.send({ message: "Server is up and running" });
 });
 
-let getUser = async(req,res) => {
-    try{
-      let user = await User.find()
-      console.log(user)
-      res.json({
-        status : 'success',
-        data : user
-      })
-    }
-    catch(err){
-      res.status(500).json({
-        status : 'Failed',
-        message : err.message
-      })
-    }
-}
+let signUp = async (req, res) => {
+  try {
+    let { Name, Email, Mobile, Password } = req.body;
 
-  let postFunction = async(req,res) => {
-    try{
-      console.log(req.body)
-      let {Name, Email, Mobile } = req.body;
-      await User.create({Name,Email,Mobile});
-      res.json({
-        status: 'Suucees',
-        message: 'User created Successfully'
-      })
-    }
-    catch(err){
-      res.status(500).json({
-        status: 'Failed',
-        message : err.message
-      })
+    const duplicateVerification = await User.findOne({ Email });
+    //  console.log(duplicateVerification)
+    if (duplicateVerification) {
+      return res.status(500).json({
+        status: "Failed to create",
+        message: "Mail id is alredy used",
+      });
     }
 
+    let salt = await bcrypt.genSalt(10);
+    let encryptedPassword = await bcrypt.hash(Password, salt);
+    const userDetails = { Name, Email, Mobile, Password: encryptedPassword };
+    await User.create(userDetails);
+
+    const token = jwt.sign(userDetails, process.env.JWT_SECRET, {
+      expiresIn: 120,
+    });
+
+    res.json({
+      status: "Suucees",
+      message: "User created Successfully",
+      token,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Failed",
+      message: err.message,
+    });
   }
-  app.post('/', postFunction)
+};
 
-  app.get('/user', getUser)
+const signIn = async (req, res) => {
+  try {
+    const { Email, Password } = req.body;
+    let user = await User.findOne({ Email });
+
+    if (!user) {
+      return res.json({
+        status: "Failed",
+        message: "email is wrong",
+      });
+    }
+
+    const passwordMatched = await bcrypt.compare(Password, user.Password);
+
+    if (!passwordMatched) {
+      return res.json({
+        status: "Failed",
+        message: " Password is wrong!",
+      });
+    }
+    const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+      expiresIn: 120,
+    });
+    res.json({
+      status: "Success",
+      message: `Welcome ${user.Name}`,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "Failed",
+      message: "User doesn't exist",
+    });
+  }
+};
+
+app.post("/SignUp", signUp);
+
+app.post("/signIn", signIn);
 let PORT = process.env.PORT;
 let connect = mongoose
-                        .connect(process.env.MONGODB_URL)
-                        .then(() => {
-                            console.log(`server running on ${PORT}`)
-                        })
-                        .catch((error) => {console.log(error)})
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log(`server running on ${PORT}`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(PORT, () => {
-  connect
+  connect;
 });
