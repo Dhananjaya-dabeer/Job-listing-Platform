ec4ed77b12523fb6648f9e35f96009653ebd1838
Dhananjaya-dabeer <dabeerdhananja123@gmail.com>
2023-09-18 16:50:02 +0530

    Added error handler middleware

--- node.js
+++ node.js
@@ -11,6 +11,7 @@ dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
 app.get("/", (req, res) => {
   res.status(200).send("go to /health to confirm the server is working");
 });
@@ -94,6 +95,21 @@ const signIn = async (req, res) => {
 app.post("/SignUp", signUp);
 
 app.post("/signIn", signIn);
+
+app.use((req,res,next) => {
+  const error = new Error("Not Found")
+  error.status = 404
+  next(error)
+})
+
+// error handler
+app.use((error,req,res,next) => {
+  res.status(error.status || 500)
+  res.send({
+    status : error.status || 500,
+    message : error.message
+  })
+})
 let PORT = process.env.PORT;
 let connect = mongoose
   .connect(process.env.MONGODB_URL)
